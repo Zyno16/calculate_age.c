cede0763b9f1aa6a925ec5f25d4464f0eb3d49d3
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-08-12 06:56:23 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int age;
+int year_of_birth;
+printf("wich year are you born?:\n");
+scanf("%d",&year_of_birth);
+age=(2023-year_of_birth);
+printf("your are %d years old\n",age);
+
+
+
+
+    return 0;
+}
